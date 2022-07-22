4cc461887b63dd01bb56446bfab19a1abb4de769
imronha <imronha@gmail.com>
2022-07-21 22:01:37 -0700

    URL placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
 import Products from "./Products";
-
+import { Routes, Route} from 'react-router-dom';
+import Detail from "./Detail"; 
+import Cart from "./Cart";
 
 export default function App() {
 
@@ -12,7 +14,12 @@ export default function App() {
       <div className="content">
         <Header />
         <main>
-          <Products></Products>
+          <Routes>
+            <Route path="/" element={<h1> Welcome to Carved Rock fitness</h1>}></Route>            
+            <Route path="/:category" element={<Products/>}></Route>
+            <Route path="/detail" element={<Detail/>}></Route>
+            <Route path="/cart" element={<Cart/>}></Route>
+          </Routes>
         </main>
       </div>
       <Footer />
