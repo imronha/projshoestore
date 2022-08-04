6b663adeacde1f6eee5e6e0562c5f467c20a6061
imronha <imronha@gmail.com>
2022-08-03 21:04:22 -0700

    Updated detail.jsx to have size selector

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, {useState} from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "./services/useFetch";
 import Spinner from "./Spinner";
 import PageNotFound from "./services/PageNotFound";
@@ -7,6 +7,8 @@ import PageNotFound from "./services/PageNotFound";
 
 export default function Detail() {
     const { id } = useParams();
+    const navigate = useNavigate();
+    const [sku, setSku] = useState("");
 
     const { data:product, loading, error} = useFetch(`products/${id}`);
 
@@ -23,6 +25,17 @@ export default function Detail() {
             <h1>{product.name}</h1>
             <p>{product.description}</p>
             <p id="price">${product.price}</p>
+            <select id="size" value={sku} onChange={ (e)=> setSku(e.target.value)}>
+              <option value="">What Size?</option>
+              { product.skus.map((s) => (
+                  <option key={s.sku} value={s.sku}>
+                      {s.size}
+                  </option>
+              ))}
+            </select>
+            <p>
+                <button className="btn btn-primary" onClick={() => navigate("/cart")}>Add to cart</button>
+            </p>
             <img src={`/images/${product.image}`} alt={product.category} />
         </div>
     );
