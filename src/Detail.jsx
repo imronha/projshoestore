3a94c00a825d5752c745c47bd442a761485dd21f
imronha <imronha@gmail.com>
2022-08-03 20:34:29 -0700

    updated detail.jsx

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,15 +1,29 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "./services/useFetch";
+import Spinner from "./Spinner";
+import PageNotFound from "./services/PageNotFound";
 
 
 export default function Detail() {
-    return <h1>Detail</h1>
+    const { id } = useParams();
+
+    const { data:product, loading, error} = useFetch(`products/${id}`);
+
+    if (loading) return <Spinner></Spinner>;
+    if (!product) {
+        return <PageNotFound></PageNotFound>;
+    }
+    if (error) {
+        throw error;
+    }
+
+    return (
+        <div id="detail">
+            <h1>{product.name}</h1>
+            <p>{product.description}</p>
+            <p id="price">${product.price}</p>
+            <img src={`/images/${product.image}`} alt={product.category} />
+        </div>
+    );
 }
-// TODO: Display these products details
-// return (
-// <div id="detail">
-//     <h1>{product.name}</h1>
-//     <p>{product.description}</p>
-//     <p id="price">${product.price}</p>
-//     <img src={`/images/${product.image}`} alt={product.category} />
-// </div>
-// );
