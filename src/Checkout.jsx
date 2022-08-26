d9379546452f072e1340f174e3a5df60cff177ee
imronha <imronha@gmail.com>
2022-08-25 20:31:56 -0700

    Update Checkout.jsx

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -19,6 +19,7 @@ export default function Checkout({ cart, emptyCart }) {
   const [address, setAddress] = useState(emptyAddress);
   const [status, setStatus] = useState(STATUS.IDLE);
   const [ saveError, setSaveError] = useState(null);
+  const [touched, setTouched] = useState({});
 
   // Derived state
   const errors = getErrors(address);
@@ -37,8 +38,12 @@ export default function Checkout({ cart, emptyCart }) {
     })
   }
 
+
   function handleBlur(event) {
-    // TODO
+    event.persist();
+    setTouched( (current) => {
+      return {...current, [event.target.id]: true};
+    })
   }
 
   async function handleSubmit(event) {
@@ -98,6 +103,9 @@ export default function Checkout({ cart, emptyCart }) {
             onBlur={handleBlur}
             onChange={handleChange}
           />
+          <p role="alert">
+            {(touched.city || status === STATUS.SUBMITTED) && errors.city}
+          </p>
         </div>
 
         <div>
@@ -115,6 +123,9 @@ export default function Checkout({ cart, emptyCart }) {
             <option value="United Kingdom">United Kingdom</option>
             <option value="USA">USA</option>
           </select>
+          <p role="alert">
+            {(touched.country || status === STATUS.SUBMITTED) && errors.country}
+          </p>
         </div>
 
         <div>
