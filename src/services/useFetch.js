c71d08f361493b0f92ffc1a7e1aa64b52bf0afd8
imronha <imronha@gmail.com>
2022-09-08 20:45:42 -0700

    Update useFetch.js

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -8,16 +8,18 @@ https://app.pluralsight.com/course-player?clipId=e024d15c-dcf7-4608-81ad-0d41802
 
 */
 
-import {useState, useEffect} from 'react';
+import {useState, useRef, useEffect} from 'react';
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 export default function useFetch(url) {
+    const isMounted = useRef(false);
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        isMounted.current = true;
         async function init() {
           try {
             const response = await fetch(baseUrl + url)
@@ -28,12 +30,15 @@ export default function useFetch(url) {
                 throw response;
             }
           } catch (e) {
-            setError(e);
+            if (isMounted.current) setError(e);
           } finally {
-           setLoading(false);
+           if (isMounted.current) setLoading(false);
           }
         }
         init();
+        return () => {
+          isMounted.current = false;
+        }
       }, [url])
 
       return {data, error, loading};
